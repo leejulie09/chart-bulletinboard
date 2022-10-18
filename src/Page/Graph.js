7741b2539b6483ae2a6ae6b765518246249e3176
Julie Lee <leejulie09@gmail.com>
2022-10-18 21:58:23 +0900

    Add: 그래프 세개 구현

--- src/Page/Graph.js
+++ src/Page/Graph.js
@@ -1,12 +1,26 @@
-import GraphChildren from "../Component/GraphPageComponent/GraphChildren";
+import BarChart from "../Component/GraphPageComponent/BarChart";
+import PieChart from "../Component/GraphPageComponent/PieChart";
+import LineChart from "../Component/GraphPageComponent/LineChart";
 import Template from "../Template/Template";
+import styled from "styled-components";
 
 function Graph() {
   return (
     <Template>
-      <GraphChildren />
+      <Wrapper>
+        <BarChart />
+        <PieChart />
+        <LineChart />
+      </Wrapper>
     </Template>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  margin: 100px 40px;
+  /* background-color: white; */
+  gap: 10%;
+`;
 
 export default Graph;
